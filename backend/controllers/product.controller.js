571e5b3622dc1e8d4d80f0ee8a556def18ae4fa5
matheusrodrigues1 <matech00@outlook.com>
2024-09-20 22:30:23 -0300

    product routes (CRUD)

---
+++ backend/controllers/product.controller.js
@@ -0,0 +1,155 @@
+import cloudinary from "../lib/cloudinary.js";
+import { redis } from "../lib/redis.js";
+import Product from "../models/product.model.js";
+
+export const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.json({ products });
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const getFeaturedProducts = async (req, res) => {
+  try {
+    let featuredProducts = await redis.get("featured_products");
+    if (featuredProducts) {
+      return res.json(JSON.parse(featuredProducts));
+    }
+
+    featuredProducts = await Product.find({ isFeatured: true }).len();
+
+    if (!featuredProducts) {
+      return res.status(404).json({ message: "no featured products found" });
+    }
+
+    await redis.set("featured_products".JSON.stringify(featuredProducts));
+
+    res.json(featuredProducts);
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const createProduct = async (req, res) => {
+  try {
+    const { name, description, price, image, category } = req.body;
+
+    let cloudinaryResponse = null;
+
+    if (image) {
+      cloudinaryResponse = await cloudinary.uploader.upload(image, {
+        folder: "products",
+      });
+    }
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      image: cloudinaryResponse?.secure_url
+        ? cloudinaryResponse.secure_url
+        : "",
+      category,
+    });
+
+    res.status(201).json(product);
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+      return res.status(404).json({ message: "product" });
+    }
+
+    if (product.image) {
+      const publicId = product.image.split("/").pop().split(".")[0];
+
+      try {
+        await cloudinary.uploader.destroy(`products/${publicId}`);
+
+        console.log("deleted");
+      } catch (error) {
+        console.log("error", error);
+      }
+    }
+
+    await Product.findByIdAndDelete(req.params.id);
+
+    res.json({ message: "deleted" });
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const getRecommendedProducts = async (req, res) => {
+  try {
+    const products = await Product.aggregate([
+      {
+        $sample: { size: 3 },
+      },
+      {
+        $project: {
+          _id: 1,
+          name: 1,
+          description: 1,
+          image: 1,
+          price: 1,
+        },
+      },
+    ]);
+
+    res.json(products);
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const getProductsByCategory = async (req, res) => {
+  const { category } = req.params;
+  try {
+    const products = await Product.find({ category });
+    res.json(products);
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+export const toggleFeaturedProduct = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+
+    if (product) {
+      product.isFeatured = !product.isFeatured;
+      const updateProduct = await product.save();
+      await updateFeaturedProductsCache();
+      res.json(updateProduct);
+    } else {
+      res.status(404).json({ message: "not found" });
+    }
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(500).json({ message: "error", error: error.message });
+  }
+};
+
+async function updateFeaturedProductsCache(){
+  try {
+    const featuredProducts = await Product.find({isFeatured: true}).len()
+    await redis.set('featured_products', JSON.stringify(featuredProducts))
+  } catch (error) {
+    console.log("error", error);
+  }
+}
---
+++ backend/middleware/auth.middleware.js
@@ -0,0 +1,41 @@
+import User from "../models/user.model.js";
+import jwt from "jsonwebtoken";
+
+export const protectRoute = async (req, res, next) => {
+  try {
+    const accessToken = req.cookies.accessToken;
+
+    if (!accessToken) {
+      return res.status(401).json({ message: "unauthorized" });
+    }
+
+    try {
+      const decoded = jwt.verify(accessToken, process.env.ACCESS_TOKEN_SECRET);
+      const user = await User.findById(decoded.userId).select("-password");
+
+      if (!user) {
+        return res.status(401).json({ message: "user not dound" });
+      }
+
+      req.user = user;
+
+      next();
+    } catch (error) {
+      if (error.name === "TokenExpiredError") {
+        return res.status(401).json({ message: "unauthorized expired" });
+      }
+      throw error;
+    }
+  } catch (error) {
+    console.log("error", error.message);
+    res.status(401).json({ message: "unauthorized" });
+  }
+};
+
+export const adminRoute = (req, res, next) => {
+  if (req.user && req.user.role === "admin") {
+    next();
+  } else {
+    return res.status(403).json({ message: "access denied" });
+  }
+};
